7be73d6456c2001411fe420c1c2c2974de676649
lsc <573291342@11.com>
2022-10-25 21:15:12 +0800

    10.25

--- miniprogram-1/pages/index/index.js
+++ miniprogram-1/pages/index/index.js
@@ -4,39 +4,42 @@ const app = getApp()
 
 Page({
   data: {
-    info:'hello world',
-    count:0,
-    msg:'你好'
+    info: 'hello world',
+    count: 0,
+    msg: '你好',
+    type: 1,
+    flag: true,
+    arr1: [{id: '001',name: '苹果'}, {id: '002',name: '华为'}, {id: '003',name: '小米'}]
   },
   // 定义按钮的事件处理函数
   btnTapHandler(e) {
     console.log(e);
   },
   //加一按钮的事件处理函数
-  countChange(){
+  countChange() {
     this.setData({
-      count:this.data.count+1
+      count: this.data.count + 1
     })
   },
   //加二事件处理函数
   btnTap2(e) {
     this.setData({
-      count:this.data.count+e.target.dataset.info
+      count: this.data.count + e.target.dataset.info
     })
   },
   //文本框事件处理函数
-  inputHandler(e){
+  inputHandler(e) {
     this.setData({
-      msg:e.detail.value
+      msg: e.detail.value
     })
   },
   onLoad() {
-    
+
   },
   getUserProfile(e) {
-    
+
   },
   getUserInfo(e) {
-   
+
   }
 })
